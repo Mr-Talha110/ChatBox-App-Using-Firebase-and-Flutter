f129d6c2a7f9f8d00d4ab639e9e1871cc2fbef60
talha latif <talhalatif636@gmail.com>
2023-11-24 18:17:56 +0500

    Made the home chat screen

--- lib/screens/messaging screens/messaging_screen.dart
+++ lib/screens/messaging screens/messaging_screen.dart
@@ -100,6 +100,103 @@ class _MessagingScreenState extends State<MessagingScreen> {
                             color: const Color(0xffe6e6e6))),
                   ),
                   const SizedBox(height: 25),
+                  Expanded(
+                      child: ListView.builder(
+                          itemCount: contactList.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              width: double.infinity,
+                              margin: const EdgeInsets.symmetric(vertical: 15),
+                              child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Stack(
+                                          children: [
+                                            Image.asset(
+                                              contactList[index].image,
+                                              width: 52,
+                                            ),
+                                            Positioned(
+                                              bottom: 0,
+                                              right: 0,
+                                              child: Container(
+                                                width: 10,
+                                                height: 10,
+                                                decoration: BoxDecoration(
+                                                    color: index % 2 == 0
+                                                        ? const Color(
+                                                            0xff9A9E9C)
+                                                        : const Color(
+                                                            0xff0FE16D),
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            100)),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        const SizedBox(width: 12),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              contactList[index].name,
+                                              style: const TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                            const Text("How are you today?",
+                                                style: TextStyle(
+                                                    fontSize: 12,
+                                                    fontWeight: FontWeight.w500,
+                                                    color: Color(0xff797C7B)))
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.end,
+                                      children: [
+                                        const Text(
+                                          '2 min ago',
+                                          style: TextStyle(
+                                              fontSize: 12,
+                                              fontWeight: FontWeight.w500,
+                                              color: Color(0xff797C7B)),
+                                        ),
+                                        const SizedBox(height: 7),
+                                        index == 0 || index == 1
+                                            ? Container(
+                                                width: 21,
+                                                height: 21,
+                                                decoration: BoxDecoration(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            100),
+                                                    color: const Color(
+                                                        0xffF04A4C)),
+                                                child: const Center(
+                                                    child: Text(
+                                                  '3',
+                                                  style: TextStyle(
+                                                    fontSize: 12,
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.w500,
+                                                  ),
+                                                )),
+                                              )
+                                            : Container()
+                                      ],
+                                    )
+                                  ]),
+                            );
+                          }))
                 ]),
               ),
             )
